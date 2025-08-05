12b3fbf56957dcd35514f923708f99be057a7e91
hemamalini104 <hemamalini100904@gmail.com>
2025-08-05 23:00:23 +0530

    Profile view

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -8,6 +8,8 @@ class CustomUser(AbstractUser):
         ('attendee', 'Attendee'),
     )
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='attendee')
+    phone = models.CharField(max_length=15, blank=True)
+    profile_image = models.ImageField(upload_to='profiles/', blank=True, null=True)
 
     def __str__(self):
         return f"{self.username} - {self.role}"
--- server/accounts/views.py
+++ server/accounts/views.py
@@ -3,9 +3,20 @@ from rest_framework import generics
 from .serializers import RegisterSerializer
 from rest_framework.permissions import AllowAny
 from .models import CustomUser
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .serializers import ProfileSerializer
+
 
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
 
