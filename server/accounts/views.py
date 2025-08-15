de67b1cf865b1cf6412f2790ceb5da5d7727ea81
hemamalini104 <hemamalini100904@gmail.com>
2025-08-15 11:33:50 +0530

    Pull changes

--- server/accounts/views.py
+++ server/accounts/views.py
@@ -6,6 +6,7 @@ from .models import CustomUser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .serializers import ProfileSerializer
+from rest_framework.views import APIView
 
 
 class RegisterView(generics.CreateAPIView):
